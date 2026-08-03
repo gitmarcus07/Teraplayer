8a3a01f97877763ba6794d18d52fd6081cd6cb79
Marcus <allgamer7172@gmail.com>
2026-08-03 11:39:12 +0530

    Final logo size

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -36,7 +36,7 @@ export default function Header({ onOpenHistory, onOpenFavorites }) {
             <img
               src={logo}
               alt="TeraPlayer"
-              className="h-10 md:h-14 w-auto"
+              className="h-16 md:h-18 w-auto"
             />
           </Link>
 
@@ -202,7 +202,7 @@ export default function Header({ onOpenHistory, onOpenFavorites }) {
                     <img
                       src={logo}
                       alt="TeraPlayer"
-                      className="h-17 w-auto"
+                      className="h-20 w-auto"
                     />
                   </Link>
 
